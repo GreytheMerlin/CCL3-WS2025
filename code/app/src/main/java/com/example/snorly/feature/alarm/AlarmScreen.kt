02b855e60ed3471ed63c2254aa2d5030774ccb6e
Laurens Türk <laurenstuerk@gmail.com>
2026-01-12 19:43:50 +0100

    app top bar

--- code/app/src/main/java/com/example/snorly/feature/alarm/AlarmScreen.kt
+++ code/app/src/main/java/com/example/snorly/feature/alarm/AlarmScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.snorly.core.common.components.HomeTopBar
 import com.example.snorly.feature.alarm.components.AlarmCard
 
 @Composable
@@ -25,12 +27,18 @@ fun AlarmScreen(
     // Whenever the list changes in the ViewModel, 'alarms' will update here
     val alarms by viewModel.alarms.collectAsState()
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
+    Scaffold(
+        topBar = {
+            HomeTopBar(title = "Alarm")
+        }
+    ) {innerPadding ->
         LazyColumn(
-            contentPadding = PaddingValues(16.dp),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                end = 16.dp,
+                top = innerPadding.calculateTopPadding(),
+                bottom = innerPadding.calculateBottomPadding()
+            ),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier.fillMaxSize()
         ) {
